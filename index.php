1675b46b2510da43e926e645334f3c3d1d89043d
Dan <danpopa1998@gmail.com>
2018-09-28 03:17:59 +0300

    make login & logout

--- index.php
+++ index.php
@@ -4,26 +4,44 @@ require_once 'admin/config.php';
 require_once 'admin/functions.php';
 dbConnect();
 ?>
+
+<?
+
+
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    $_SESSION['authorized'] = (
+        $_POST['username'] === $APP['admin_panel_access']['username']
+        &&
+        $_POST['password'] === $APP['admin_panel_access']['password']
+    );
+}
+if ($_GET['module'] === 'auth' && $_GET['action'] === 'logout') {
+    unset($_SESSION);
+}
+
+?>
+
 <!doctype html>
 <html>
 <head>
     <title></title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <?
-    include 'pages/blocks/menu.php';
-    ?>
-    <hr>
-    <?
+<?
+include 'pages/blocks/menu.php';
+?>
+<hr>
+<?
 
-    $module = $_GET['module'] ?: 'default';
-    $action = $_GET['action'] ?: 'home';
+$module = $_GET['module'] ?: 'default';
+$action = $_GET['action'] ?: 'home';
 
-    if(file_exists("pages/{$module}/{$action}.php"))
-        include "pages/{$module}/{$action}.php";
-    else
-        include "pages/error/404.php";
-    ?>
+if (file_exists("pages/{$module}/{$action}.php"))
+    include "pages/{$module}/{$action}.php";
+else
+    include "pages/error/404.php";
+?>
 </body>
 </html>
--- pages/blocks/menu.php
+++ pages/blocks/menu.php
@@ -1,1 +1,6 @@
 <a href="?module=books&action=list">List of Books</a>
+<? if(!$_SESSION['authorized']) { ?>
+<a href="?module=auth&action=login">Login</a>
+<?} else { ?>
+<a href="?module=auth&action=logout">Logout</a>
+<?}?>
